e46deebd582f0251c4c7abeff11162f3f5511df9
walid998112 <didouplay386@gmail.com>
2024-12-15 14:28:02 +0100

    ws2

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import ListPlayers from './Components/ListPlayers';
+
+function App() {
+  return (
+    <div>
+        <ListPlayers/>
+    </div>
+  );
+}
+export default App;
---
+++ src/players.js
@@ -0,0 +1,32 @@
+// name, team, nationality, jerseyNumber, age, and an Image URL for the player.
+const players = [
+    {
+        name : "Lionel Messi",
+        team : "Inter Miami CF",
+        nationality : "Argentine",
+        jerseyNumber : 10,
+        age : 37,
+        Image : "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Lionel_Messi.png/375px-Lionel_Messi.png"
+
+    },
+    {
+        name : "Issam Jemâa",
+        team : "Dubaï Club",
+        nationality : "Tunisien",
+        jerseyNumber : 13,
+        age : 40,
+        Image : "https://img.a.transfermarkt.technology/portrait/big/31430-1594108516.jpg?lm=1"
+
+    },
+    {
+        name : "Hannibal Mejbri",
+        team : "Burnley FC",
+        nationality : "Française",
+        jerseyNumber : 14,
+        age : 21,
+        Image : "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Manchester_United_v_Norwich_City%2C_16_April_2022_%2802%29.jpg/420px-Manchester_United_v_Norwich_City%2C_16_April_2022_%2802%29.jpg"
+
+    }
+]
+
+export default players
